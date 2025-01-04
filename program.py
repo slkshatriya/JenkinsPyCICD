440f194a1c861af3acc2ccc207ca45cc14cb4980
slkshatriya <surajkeshr023@gmail.com>
2025-01-04 21:50:19 +0530

    Added test cases

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
-list = [5,3,1,4,2]
+list = [5, 3, 1, 4, 2]
 
-list.sort()
-print("This is a sorted list", list)
-list.reverse()
-print("This is a reverse of sorted list", list)
+
+def sortedList():
+    return list.sort()
+
+
+def reverseList():
+    return list.reverse()
---
+++ test_program.py
@@ -0,0 +1,10 @@
+from program import *
+
+
+def test_sorted():
+    assert sortedList() == [5, 4, 3, 2, 1]
+
+
+def test_reverse():
+    assert reverseList() == [1, 2, 3, 4, 5]
+
